e3cfe14bb63ddd850ec7d4eb471bee3e96e51f9d
Stoian Sorin <stoiansorinalexandru@gmail.com>
2025-07-29 20:42:07 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import './Board.js'
+
+function App() {
+  return (
+    <div>
+      <Board></Board>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Board.js
@@ -0,0 +1,10 @@
+import "react"
+
+function Board(){
+    
+    return (
+        <>
+
+        </>
+    )
+}
