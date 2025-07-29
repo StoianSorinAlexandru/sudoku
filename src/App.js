1e524fb27caf1266639da90a10d013de312ce20f
Stoian Sorin <stoiansorinalexandru@gmail.com>
2025-07-29 22:44:42 +0300

    Added the table

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
-import './Board.js'
 
 function App() {
   return (
@@ -10,4 +9,70 @@ function App() {
   );
 }
 
+function Board(){
+    
+    return (
+        <>
+          <div className='board'>
+            <Square></Square>
+          </div>
+        </>
+    )
+}
+
+function Square(){
+
+  // useState(squares, setSqares) = 
+  
+  return (
+      <>
+        <div className='game'>
+          <div className='board-row'>
+            <Cell></Cell>
+            <Cell></Cell>
+            <Cell></Cell>
+          </div>
+          
+          <div className='board-row'>
+            <Cell></Cell>
+            <Cell></Cell>
+            <Cell></Cell>
+          </div>
+          <div className='board-row'>
+            <Cell></Cell>
+            <Cell></Cell>
+            <Cell></Cell>
+          </div>
+        </div>
+      </>
+    )
+}
+
+function Cell(){
+  return (
+    <>
+      <div>
+        <Row></Row>
+        <Row></Row>
+        <Row></Row>    
+      </div>
+    </>
+  )
+}
+
+
+
+function Row(){
+  return (
+      <>
+        <div className='board-row'>
+          <button className="square"></button>
+          <button className="square"></button>
+          <button className="square"></button>
+        </div>
+      </>
+    )
+}
+
 export default App;
+
